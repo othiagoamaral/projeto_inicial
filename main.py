5346a9e360801bb22d7af33059cdcdd120e4b5cd
Thiago <thiago10ma@gmail.com>
2024-12-06 13:02:53 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Jornada de dados")
+
+# Variáveis
+idade = 25
+nome = "Thiago"
+
+# Print
+print(idade)
+
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Listas
+lista = [1,2,3,4,5]
+
+# Métodos
+lista_invertida = lista.reverse()
